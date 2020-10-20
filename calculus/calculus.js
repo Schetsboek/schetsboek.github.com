19c2108465237cb1edcb03a8fdb55fb8de3185db
Schetsboek <jeroen.schets@gmail.com>
2020-10-20 13:45:21 +0200

    Add files via upload

--- calculus/calculus.js
+++ calculus/calculus.js
@@ -1,8 +1,10 @@
-let passedCalculus = false;
+let passedCalculus = null;
 
 let passedCalculus_field = document.getElementById('passedCalculus');
 if(passedCalculus){
 	passedCalculus_field.textContent = "JA!";
+} else if(passedCalculus == null){
+	passedCalculus_field.textContent = "MAYBE";
 } else {
 	passedCalculus_field.textContent = "NEE";
 }
