a3127fc73a49c74e6746725664ee2c07b4cccdec
Schetsboek <jeroen.schets@gmail.com>
2022-04-05 22:03:33 +0200

    light modus

--- verjaardag/verlanglijstje.js
+++ verjaardag/verlanglijstje.js
@@ -10,3 +10,26 @@ function leave(event){
 		iterables[i].style.animation = '';
 	}
 }
+
+let lightsOn = false;
+
+function lights(event) {
+	const switchElement = document.getElementById("switch");
+	
+	if (lightsOn) {
+		switchElement.style.transform = "rotate(0deg)";
+		
+		document.documentElement.style.background = "linear-gradient(135deg, rgba(2,57,164,1) 0%, rgba(0,212,255,1) 100%)";
+		document.documentElement.style.color = "#000";
+		document.documentElement.style.text_shadow = "0 0 2px #405eb1";
+		
+	} else {
+		switchElement.style.transform = "rotate(180deg)";
+		
+		document.documentElement.style.background = "white";
+		document.documentElement.style.color = "#e7e7e7";
+		document.documentElement.style.text_shadow = "0 0 2px yellow";
+	}
+	
+	lightsOn = !lightsOn;
+}
